52bf1788fe995e41640c1e83c07c76589da6de1c
Islomov-Doniyorbek <159924043+Islomov-Doniyorbek@users.noreply.github.com>
2024-09-17 21:12:36 +0500

    Add files via upload

---
+++ Authentication/auth.js
@@ -0,0 +1,99 @@
+let forms = document.querySelectorAll("form");
+
+// Formlarni bir biriga bog'laydigan button va linklar
+let logHave = document.querySelector("form .logHave #link"),
+    DontHave = document.querySelector("form .DontLog #link"),
+    zabil = document.querySelector("form #zabil"),
+    sendCodePhone = document.querySelector("form #sendCodePhone"),
+    sendCode = document.querySelector("form #sendCode"),
+    sendNewPassword = document.querySelector("form #sendNewPassword"),
+    resend = document.querySelector("form #resend"),
+    registr = document.querySelector("form #registr");
+
+registr.onclick = ()=> {
+    let ism = document.querySelector(".ism").value;
+
+    localStorage.setItem("user", ism)
+}
+
+logHave.onclick = (e)=> {
+    e.preventDefault();
+    forms.forEach(e => {
+        if (e.id != "formLog") {
+            e.style.display = "none"
+        } else {
+            e.style.display = "flex"
+        }
+    })
+}
+DontHave.onclick = (e)=> {
+    e.preventDefault();
+    forms.forEach(e => {
+        if (e.id != "formReg") {
+            e.style.display = "none"
+        } else {
+            e.style.display = "flex"
+        }
+    })
+}
+zabil.onclick = (e)=> {
+    e.preventDefault();
+    forms.forEach(e => {
+        if (e.id != "formZabil") {
+            e.style.display = "none"
+        } else {
+            e.style.display = "flex"
+        }
+    })
+}
+sendCodePhone.onclick = (e)=> {
+    e.preventDefault();
+    forms.forEach(e => {
+        if (e.id != "formSMS") {
+            e.style.display = "none"
+        } else {
+            e.style.display = "flex"
+        }
+    })
+    let times = document.querySelector(".times");
+    let counter = 59;
+    let intervalId = setInterval(function () {
+        times.innerHTML = `0:${counter}`
+        counter--;
+        
+        if (counter <=0) {
+          clearInterval(intervalId);
+        }
+      }, 1000); 
+}
+sendCode.onclick = (e)=> {
+    e.preventDefault();
+    forms.forEach(e => {
+        if (e.id != "formNewPass") {
+            e.style.display = "none"
+        } else {
+            e.style.display = "flex"
+        }
+    })
+}
+sendNewPassword.onclick = (e)=> {
+    e.preventDefault();
+    forms.forEach(e => {
+        if (e.id != "formLog") {
+            e.style.display = "none"
+        } else {
+            e.style.display = "flex"
+        }
+    })
+}
+
+let input = document.querySelectorAll("input");
+input.forEach(e => {
+    e.addEventListener('input', () => {
+        if (e.validity.valid) {
+          e.parentElement.style.boxShadow = '0 0 8px 3px green'; // Valid holat uchun yashil rang
+        } else {
+            e.parentElement.style.boxShadow = '0 0 8px 3px red'; // Valid holat uchun yashil rang
+        }
+      });
+})
---
+++ investor/invest.js
@@ -0,0 +1,61 @@
+let dovom = document.querySelector("#dovom");
+let wrapper = document.querySelector(".wrapper")
+let title = document.querySelector(".title")
+dovom.onclick = () => {
+    wrapper.innerHTML = `
+    <div class="selects">
+        <p style="width: 100%;">O'zingiz xohlagan biznes yo'nalishini tanlang</p>
+        <select id="typeBusiness" name="">
+            <option value="default">Tanlash</option>
+            <option value="Ta'lim">Ta'lim</option>
+            <option value="Ishlab chiqarish">Ishlab chiqarish</option>
+            <option value="Qishloq xo'jaligi">Qishloq xo'jaligi</option>
+            <option value="Kelajak kasblari">Kelajak kasblari</option>
+            <option value="Sog'liqni saqlash">Sog'liqni saqlash</option>
+            <option value="Ekologiya">Ekologiya</option>
+            <option value="Ilm-fan">Ilm-fan</option>
+            <option value="Savdo ishlari">Savdo ishlari</option>
+            <option value="Qurilish">Qurilish</option>
+            <option value="Boshqa">Boshqa</option>
+        </select>
+        <p style="width: 100%;">Loyihaga ajratmoqchi bo'lgan mablag'ni kiriting <br>($ miqdorda)</p>
+        <select id="investitsiya" name="">
+            <option value="default">Tanlash</option>
+            <option value="100$ - 500$">100$ - 500$</option>
+            <option value="500$ - 1000$">500$ - 1000$</option>
+            <option value="1000$ - 5000$">1000$ - 5000$</option>
+            <option value="5000$ - 10000$">5000$ - 10000$</option>
+            <option value="10000$ - 20000$">10000$ - 20000$</option>
+            <option value="20000$ - 50000$">20000$ - 50000$</option>
+            <option value="50000$ - 100000$">50000$ - 100000$</option>
+            <option value="100000$ - 200000$">100000$ - 200000$</option>
+            <option value="200000$ - undan yuqori">200000$ - undan yuqori</option>
+        </select>
+        <p>Siz uchun loyihani amalga oshirish qancha muddat talab qilishi kerak</p>
+        <select id="muddat" name="">
+            <option value="default">Tanlash</option>
+            <option value="3 oygacha">3 oygacha</option>
+            <option value="3 - 6 oy">3 - 6 oy</option>
+            <option value="6 oydan 1 yilgacha">6 oydan 1 yilgacha</option>
+            <option value="1 - 5 yil">1 - 5 yil</option>
+            <option value="5 - 10 yil">5 - 10 yil</option>
+            <option value="10 - 20 yil">10 - 20 yil</option>
+            <option value="Butun umr">Butun umr</option>
+        </select>
+        <br>
+        <button id="qidiruv">Qidirish</button>
+    </div>
+    `;
+    title.style.display = "none"
+    wrapper.classList.toggle("selects_out")
+    let qidiruv = document.querySelector("#qidiruv");
+    qidiruv.onclick = () => {
+        let selectss = document.querySelectorAll(".selects select");
+        selectss.forEach(e => {
+            if (e.value == "default") {
+                e.style.borderBottom = "2px solid aqua"
+            }
+        })
+        console.log(123);
+    }
+}
