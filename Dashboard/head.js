781c3dfadab5c764305908689480ca3d8d95e407
Islomov-Doniyorbek <159924043+Islomov-Doniyorbek@users.noreply.github.com>
2024-09-25 23:56:06 +0500

    Add files via upload

--- Dashboard/head.js
+++ Dashboard/head.js
@@ -1,9 +1,43 @@
 function profileName() {
     if (localStorage.getItem("nick")) {
         document.querySelector("#login").style.display = "none"
-        document.querySelector("#sign").style.display = "none";
+        // document.querySelector("#sign").style.display = "none";
         let name = document.querySelector(".name")
         name.innerHTML = localStorage.getItem("nick")
     }
 }
 profileName()
+// localStorage.clear()
+// console.log(document.querySelector("body").innerText);
+function translate() {
+    const select = document.querySelector("#lang");
+    const body = document.querySelector("header");
+    const countries = {
+        "en-GB":"English",
+        "ru-Ru":"Russian",
+        "uz-Uz":"Uzbek"
+    }
+    let fromTrans = select.value;
+    let toTrans;
+    let t = document.querySelectorAll(".tt")
+    console.log(t);
+    // for (let i = 0; i < t.length; i++){
+    //     console.log(t[i].innerText);
+    // }
+    select.addEventListener("change", () => {
+        toTrans = select.value;
+        // console.log(body.innerText);
+        for (let i = 0; i < t.length;i++) {
+            // console.log(tt);
+        
+
+            let api = `https://api.mymemory.translated.net/get?q=${t[i].innerText}!&langpair=${fromTrans}|${toTrans}`;
+            fetch(api).then(res => res.json()).then(data => {
+                console.log(data);
+                t[i].innerText = data.responseData.translatedText;
+            })
+            
+        }
+    })
+}
+translate()
